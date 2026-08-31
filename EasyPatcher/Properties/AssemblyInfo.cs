9f0815c6f584a2df020d19c098c188a875ffe805
hwakeyeTW <102696847+hwakeyeTW@users.noreply.github.com>
2026-08-31 08:00:17 +0800

    Use Taiwan wording in EasyPatcher metadata

--- EasyPatcher/Properties/AssemblyInfo.cs
+++ EasyPatcher/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Easy Patcher")]
-[assembly: AssemblyDescription("MAGES 自動補丁工具")]
+[assembly: AssemblyDescription("MAGES 自動修補工具")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FENGberd / hwakeyeTW")]
 [assembly: AssemblyProduct("EasyPatcher")]
